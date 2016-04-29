81b02c5a62c141d0188f8b8aaa48d076ed637f26
Reiter, Justin C <jreiter@amfam.com>
2016-04-29 14:25:59 -0500

    Chapter 6

---
+++ chapter6/api/app/models/product.js
@@ -0,0 +1,11 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var ProductSchema   = new Schema({
+    name: String,
+    description: String,
+    category: String,
+    price: Number
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
