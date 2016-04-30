d4075e1f4028ea9686057ace26f2b9e4d2a6922d
Reiter, Justin C <jreiter@amfam.com>
2016-04-30 14:00:16 -0500

    Chapter 6

--- chapter6/api/Product.js
+++ chapter6/api/Product.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 
-var productSchema = new mongoose.Schema({
+var ProductSchema = new mongoose.Schema({
     category: String,
     description: String,
     name: String,
     price: Number
 });
 
-module.exports = mongoose.model('Products', productSchema, 'Products');
+module.exports = mongoose.model('Products', ProductSchema, 'Products');
--- chapter6/api/server.js
+++ chapter6/api/server.js
@@ -3,12 +3,16 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // database stuff
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://test:pass@ds041150.mlab.com:41150/proangularjs');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-var ProductModel = require('./Product.js');
+var Product = require('./Product.js');
 
 router.use(function (req, res, next) {
     console.log('Something is happening');
@@ -21,9 +25,22 @@ router.get('/', function (req, res) {
 
 router.route('/products')
     .get(function (req, res) {
-        ProductModel.find(function(err, products) {
+        Product.find(function(err, products) {
             if (err) res.send(err);
             res.json(products);
+        });
+    })
+
+    .post(function(req, res) {
+        var product = new Product();
+        product.category = req.body.category;
+        product.name = req.body.name;
+        product.description = req.body.description;
+        product.price = req.body.price;
+
+        product.save(function (err) {
+            if (err) res.send(err);
+            res.json({message: 'Product created!'});
         });
     });
 
