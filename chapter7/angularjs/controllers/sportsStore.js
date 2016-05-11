aadf0f2859a40c717b1987ae57a0360f046cd829
Reiter, Justin C <jreiter@amfam.com>
2016-05-11 15:11:02 -0500

    chapter 7

--- chapter7/angularjs/controllers/sportsStore.js
+++ chapter7/angularjs/controllers/sportsStore.js
@@ -7,6 +7,7 @@ angular.module('sportsStore')
                 $scope.data.products = data;
             })
             .error(function(error) {
+                console.log(error);
                 $scope.data.error = error;
             });
     });
